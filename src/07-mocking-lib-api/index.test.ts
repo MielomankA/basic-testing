bf5f97ce932ad4aca5ed0ab887f1cbc84409aca1
MielomankA <mielomanka@mail.ru>
2025-11-07 14:49:05 +0300

    feat: add mocking-lib-api tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,51 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+jest.mock('lodash', () => ({
+  throttle: (fn: (...args: unknown[]) => unknown) => fn,
+}));
+
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    mockedAxios.create.mockReturnValue({
+      get: jest
+        .fn()
+        .mockResolvedValue({ data: { id: 1, title: 'Test title' } }),
+    } as unknown as typeof axios);
+
+    const result = await throttledGetDataFromApi('/posts/1');
+
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
+    expect(result).toEqual({ id: 1, title: 'Test title' });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const mockGet = jest.fn().mockResolvedValue({ data: { id: 1 } });
+    mockedAxios.create.mockReturnValue({
+      get: mockGet,
+    } as unknown as typeof axios);
+    await throttledGetDataFromApi('/posts/1');
+
+    expect(mockedAxios.create).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
+
+    expect(mockGet).toHaveBeenCalledWith('/posts/1');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockCreate = jest.fn().mockReturnValue({
+      get: jest.fn().mockResolvedValue({ data: { id: 1 } }),
+    });
+    (axios.create as jest.Mock).mockImplementation(mockCreate);
+
+    const result = await throttledGetDataFromApi('/posts/1');
+
+    expect(result).toEqual({ id: 1 });
   });
 });
